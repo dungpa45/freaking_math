9e5b4d7f15e863fde2dfa2ef716fd56a7bd43c6d
DungPham <dphamanh45@gmail.com>
2018-12-22 02:58:22 +0700

    f_math

--- app.py
+++ app.py
@@ -12,30 +12,41 @@ def home():
   if request.method == "GET":
     return render_template("freaking_math.html")
   elif request.method == "POST":
-    print('hihi')
     form = request.form
-    print(form)
     if 'easy' in form:
-      return render_template("play.html", mode = 'EASY')
+      return redirect(url_for('play', mode = 'EASY', name = form['name']))
+      # return render_template("play.html", mode='EASY', name=form['name'])
     elif 'normal' in form:
-      return render_template("play.html", mode = 'NORMAL')
+      return redirect(url_for('play', mode = 'NORMAL', name = form['name']))
+      # return render_template("play.html", mode='NORMAL', name=form['name'])
     elif 'hard' in form:
-      return render_template("play.html", mode = 'HARD')
+      return redirect(url_for('play', mode = 'HARD', name = form['name']))
+      # return render_template("play.html", mode='HARD', name=form['name'])
+    
+    
     
 
 
+@app.route("/play/<mode>/<name>", methods=['GET', 'POST'])
+def play(mode, name):
+  if request.method == 'GET':
+    return render_template("play.html", mode = mode, name = name)
+  elif request.method == 'POST':
+    form = request.form
+    point = form['point']
+    h = Highscore(name=name,level=mode,diem=point)
+    h.save()
+    return redirect(url_for('highscore'))
 
-    
 
-@app.route("/play")
-def play():
-  return render_template("play.html")
-
-
-
-@app.route("/highscore")
+@app.route("/highscore", methods=['GET', 'POST'])
 def highscore():
+  # if request.method == 'POST':
+  #   print(request.form)
+  # return "abc"
+    # print(request.form)
   hs = Highscore.objects()
+  print(hs)
   return render_template("highscore.html",hs_list=hs)
 
 if __name__ == '__main__':
